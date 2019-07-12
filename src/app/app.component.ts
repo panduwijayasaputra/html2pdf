eb88e84d02ef08443437b0977ff65434f8f10e4a
panduwijayasaputra <panduwijayasaputra@gmail.com>
2019-07-12 14:05:08 +0700

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef } from '@angular/core';
+// import * as jsPDF from 'jspdf';
+// import * as html2canvas from 'html2canvas';
+import * as html2pdf from 'html2pdf.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'HTMLToPDF';
+
+  constructor() { }
+
+  downloadPDF() {
+
+    const printArea = document.getElementById('result');
+    const option = {
+      margin: 10,
+      filename: 'file.pdf',
+      image: {
+        type: 'jpeg',
+        quality: 1
+      },
+      html2canvas: {
+        scale: 2
+      },
+      jsPDF: {
+        unit: 'mm',
+        format: 'a4',
+        orientation: 'p'
+      }
+    };
+
+    html2pdf().set(option).from(printArea).save();
+
+
+    // html2canvas(printArea).then(function (canvas: HTMLCanvasElement) {
+    //   const img = canvas.toDataURL('image/jpeg', 1.0);
+    //   const doc = new jsPDF('p', 'pt', 'a4');
+    //   doc.addImage(img, 'JPG');
+    //   doc.save('test.pdf');
+    // });
+
+    // const pdf = new jsPDF('p', 'pt', 'a4');
+    // pdf.addHTML(printArea, (doc) => {
+    //   doc.save('test.pdf');
+    // });
+  }
 }
